0b61c8062e7885ea6b3dedc49f5c18036810b8c0
Daniel Dam <damd@oregonstate.edu>
2022-05-09 20:07:13 -0700

    initial workup using text only UI

--- mastermind.py
+++ mastermind.py
@@ -11,7 +11,88 @@ The feedback will be one of the three messages:
 2. "One or more numbers are correct"
 3. "All numbers are incorrect"
 
+"""
+
+import requests
+import json
+
+LOWERLIMIT = 0
+UPPERLIMIT = 7
+MAXDIGITS = 4
 
 """
+GetNums
+No params
+return: list of random numbers as strings
+"""
+def GetNums():
+    url = f"https://www.random.org/integers/?num={MAXDIGITS}&min={LOWERLIMIT}&max={UPPERLIMIT}&col=1&base=10&format=plain&rnd=new"
+    r = requests.get(url)
+    randomNumList = r.text.split()
+    print("Correct Answers:" + str(randomNumList))
+    return randomNumList
+
+def main():
+    numOfGuesses = 0
+    while numOfGuesses < 10:
+
+        if numOfGuesses == 0:
+            randomNumList = GetNums()
+
+        guesses = []
+        print(f"You are on round {numOfGuesses + 1} of 10 guesses")
+
+        i = 0
+        while i < 4:
+            print(f"Enter a number for slot # {i + 1}: ")
+            val = input()
+            int_val = int(val)
+            if i < LOWERLIMIT or i > UPPERLIMIT:
+                print("Enter a number between 0 and 7")
+                continue
+            guesses.append(val)
+            i += 1
+
+        numOfGuesses += 1
+
+
+        print(f"Your guess: {guesses}")
+
+        correctNumber = False
+        correctNumberLocation = False
+
+        numOfCorrectNumberLocation = 0
+
+        #check for correctness
+        for i in range(MAXDIGITS):
+            if guesses[i] == randomNumList[i]:
+                correctNumberLocation = True
+                numOfCorrectNumberLocation += 1
+            elif guesses[i] in randomNumList:
+                correctNumber = True
+
+        if numOfCorrectNumberLocation == MAXDIGITS:
+            print("Winner!")
+            print("Would you like to restart? (y/n)")
+            yn = input()
+            yn = yn.lower()
+            if yn == "y" or yn == "yes":
+                numOfGuesses = 0
+                continue
+            else:
+                break
+
+        if correctNumberLocation:
+            print("One or more numbers are correct and are in the correct location\n")
+        elif correctNumber:
+            print("One or more numbers are correct\n")
+        else:
+            print("All numbers are incorrect\n")
+
+
+if __name__ == "__main__":
+    main()
+
+
 
 
