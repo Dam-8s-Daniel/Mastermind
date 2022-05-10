bd9f2192bfecbea016ed8632f0ba32332e4d9a2e
Daniel Dam <damd@oregonstate.edu>
2022-05-09 18:28:16 -0700

    initial commit

---
+++ mastermind.py
@@ -0,0 +1,17 @@
+"""
+Mastermind
+
+The goal is to guess the sequence of four numbers that are randomly selected from numbers 0 - 7. Duplicates numbers are
+possible.You have 10 tries. Every round consists of guessing four numbers. After each round, you'll receive slightly
+helpful feedback on the correctness of the numbers. The feedback message will not say which numbers are correct.
+
+The feedback will be one of the three messages:
+
+1. "One or more numbers are correct and are in the correct location"
+2. "One or more numbers are correct"
+3. "All numbers are incorrect"
+
+
+"""
+
+
